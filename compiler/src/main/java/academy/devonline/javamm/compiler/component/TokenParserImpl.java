ca921ae10ad46e93adf0aaea0947806d9f02f71d
drexpert666 <sashkaro@gmail.com>
2022-11-03 23:28:44 +0300

    added compiler

---
+++ compiler/src/main/java/academy/devonline/javamm/compiler/component/TokenParserImpl.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) $today.year.devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package academy.devonline.javamm.compiler.component;
+
+import java.util.List;
+import academy.devonline.javamm.compiler.model.TokenParserResult;
+
+/**
+ * @author drExpert666
+ */
+public class TokenParserImpl implements TokenParser {
+    @Override
+    public TokenParserResult parseLine(final String line, final boolean multilineCommentStarted) {
+        final List<String> tokens = line.isEmpty() ? List.of() : List.of(line.trim().split(" "));
+        return new TokenParserResult(tokens, false);
+    }
+}
--- compiler/src/main/java/module-info.java
+++ compiler/src/main/java/module-info.java
@@ -3,4 +3,8 @@
  */
 module javamm.compiler {
     requires javamm.code;
+
+    exports academy.devonline.javamm.compiler;
+    exports academy.devonline.javamm.compiler.model;
+    exports academy.devonline.javamm.compiler.component;
 }
